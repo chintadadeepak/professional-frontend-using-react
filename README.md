0f599f90f853f3803d371436bdb90bbadfc9e806
deepak <chintadadeepak31@gmail.com>
2025-06-17 11:32:34 +0530

    update README and refactor App component to demonstrate React concepts and hooks

--- README.md
+++ README.md
@@ -10,3 +10,21 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+## Why do we need hooks?
+Understood the true significance of React Framework w.r.t core javascript.
+
+how multiple changes which have to be done on the UI is done through hooks of react.
+
+## Virtual DOM
+it's a virtual DOM build by react framework to sync the changes between framework and actual browser DOM.
+
+## Fibre Algorithm
+Fibre is an algorithm which was internally used by react framework to update the virtual DOM, so that whatever changes happened on the virtual DOM will be reflected on the actual DOM.
+
+## Hydration
+It's a mechanism of injecting the JS logic to handle the events after rendering actual HTML, so that events can be handled in real time through the behind processing logic written in react, at the end of the day, it's JavaScript.
+
+## Reconcillation
+It's a optimization algorithm of react, which is actually responsible for identifying the changes between DOM of react and actual and updating only those changes to achieve the concept of optimization as well.
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,62 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import { jsx as _jsx } from "react/jsx-dev-runtime.js";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h3>Exploring React!!</h3>
+    </div>
+  );
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit Google",
+};
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const username = "deepak chintada";
+
+const domElementOneUsingCoreReactLibrary = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "Click me to visit GOOGLE",
+  `${username}`
+  // JS variables will be injected into the DOM after entire tree creation
+);
+
+// const domElementTwoUsingCoreReactLibrary = React.createElement(
+//   "h3",
+
+//   "Exploring React!!"
+// );
+
+createRoot(document.getElementById("root")).render(
+  // <App />
+  // [
+  // domElementOneUsingCoreReactLibrary
+  // domElementTwoUsingCoreReactLibrary,
+  // ]
+  domElementOneUsingCoreReactLibrary
+);
+// <StrictMode>
+// reactElement
+// MyApp()
+// </StrictMode>,
+// anotherElement
