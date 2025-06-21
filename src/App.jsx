c61709500970cb9c1fa3b8aec5a59694bef70662
deepak <chintadadeepak31@gmail.com>
2025-06-21 17:43:34 +0530

    feat: integrate Tailwind CSS and create Card component
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated App component to utilize the new Card component for displaying user information.
    - Created a new Card component that displays a user image, name, and button.
    - Removed old CSS styles and replaced them with Tailwind CSS imports in index.css.
    - Updated Vite configuration to include Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Card from "./components/Card";
 function App() {
-  const username = "deepak chintada";
-  return <h1>Hello World {username}</h1>;
+  // const username = "deepak chintada";
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl text-center">
+        Hello World
+      </h1>
+      <Card username="deepak" btnText="Visit Now" />
+      <Card username="bobby" btnText="Pay Now" />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,67 @@
 import { StrictMode } from "react";
-import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+// import ReactDOM from "react";
+// import { createRoot } from "react-dom/client";
 // import { jsx as _jsx } from "react/jsx-dev-runtime.js";
 import "./index.css";
 import App from "./App.jsx";
 
-function MyApp() {
-  return (
-    <div>
-      <h3>Exploring React!!</h3>
-    </div>
-  );
-}
-
-const reactElement = {
-  type: "a",
-  props: {
-    href: "https://google.com",
-    target: "_blank",
-  },
-  children: "Click me to visit Google",
-};
-
-const anotherElement = (
-  <a href="https://google.com" target="_blank">
-    Visit Google
-  </a>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
-const username = "deepak chintada";
+// function MyApp() {
+//   return (
+//     <div>
+//       <h3>Exploring React!!</h3>
+//     </div>
+//   );
+// }
 
-const domElementOneUsingCoreReactLibrary = React.createElement(
-  "a",
-  {
-    href: "https://google.com",
-    target: "_blank",
-  },
-  "Click me to visit GOOGLE",
-  `${username}`
-  // JS variables will be injected into the DOM after entire tree creation
-);
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
 
-// const domElementTwoUsingCoreReactLibrary = React.createElement(
-//   "h3",
-
-//   "Exploring React!!"
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit Google
+//   </a>
 // );
 
-createRoot(document.getElementById("root")).render(
-  // <App />
-  // [
-  // domElementOneUsingCoreReactLibrary
-  // domElementTwoUsingCoreReactLibrary,
-  // ]
-  domElementOneUsingCoreReactLibrary
-);
+// const username = "deepak chintada";
+
+// const domElementOneUsingCoreReactLibrary = React.createElement(
+//   "a",
+//   {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   "Click me to visit GOOGLE",
+//   `${username}`
+//   // JS variables will be injected into the DOM after entire tree creation
+// );
+
+// // const domElementTwoUsingCoreReactLibrary = React.createElement(
+// //   "h3",
+
+// //   "Exploring React!!"
+// // );
+
+// createRoot(document.getElementById("root")).render(
+//   // <App />
+//   // [
+//   // domElementOneUsingCoreReactLibrary
+//   // domElementTwoUsingCoreReactLibrary,
+//   // ]
+//   domElementOneUsingCoreReactLibrary
+// );
 // <StrictMode>
 // reactElement
 // MyApp()
